8e19bae7d854160ce8d73f86edf143409c385265
Dida En <den.elfsuju@gmail.com>
2021-01-13 08:55:57 +0700

    Menambahkan list pada index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     Hallo, World!
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, aliquid. Eum ut recusandae reprehenderit amet debitis quam velit id necessitatibus ex error, alias ducimus mollitia cupiditate exercitationem dolorum? Voluptates, ex.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
